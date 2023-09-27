84f37414a060ffde6aed79ab482475253f4c686d
Manucher <manucher20056@gmail.com>
2023-09-27 19:31:38 +0500

    '.'

--- src/file/storage.ts
+++ src/file/storage.ts
@@ -3,6 +3,8 @@ import { diskStorage } from 'multer'
 const getFileId = () => Math.round(Math.random() * 1e20).toString()
 
 const normalizeFileName = (req, file, callback) => {
+	file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
+
 	const fileExt = file.originalname.split('.').pop()
 	const fileName = `${getFileId()}.${fileExt}`
 
